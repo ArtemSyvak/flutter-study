8cd828ca63cde736f92c9b5e42cf1b3c2dac8b33
Artem Syvak <art.syvak@gmail.com>
2020-07-25 15:14:55 +0300

    FRON-1 :: state using.

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  var questionIndex = 0;
+
+  void answerQuestion(index) {
+    setState(() {
+      questionIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'What\'s your favorite band?',
+    ];
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('YAPIDY APP'),
         ),
-        body: Text('Discover Mountains!!!'),
+        body: Column(children: [
+          Text(questions[questionIndex]),
+          RaisedButton(
+            child: Text('Color answer'),
+            onPressed:() => answerQuestion(0),
+          ),
+          RaisedButton(
+            child: Text('Animal answer'),
+            onPressed:() => answerQuestion(1),
+          ),
+          RaisedButton(
+            child: Text('Band answer'),
+            onPressed:() => answerQuestion(2),
+          )
+        ]),
       ),
-    );    
+    );
   }
 }
